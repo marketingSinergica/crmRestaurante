5c91003812abe893633d23cad5732b099ebbf925
clararubio20 <claradeasis01@gmail.com>
2023-03-22 10:29:02 +0100

    integración de la autenticación y plantilla

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// el email tiene que ser verificado
+Auth::routes(['verify' => true ]);
+
+// Si la persona no está autentificada no va a poder acceder a la ruta home
+Route::group(['middleware' => ['auth', 'verified']], function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
