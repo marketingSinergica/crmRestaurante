c70a4da2571cd7ba7a98b9abf2002157763a02c0
clararubio20 <claradeasis01@gmail.com>
2023-03-27 09:30:33 +0200

    copia de carpeta de views de los pedidos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PedidoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 /*
@@ -26,4 +27,5 @@ Route::group(['middleware' => ['auth', 'verified'], 'as' => 'admin.'], function(
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
     Route::resource('users', UserController::class);
+    Route::resource('pedidos', PedidoController::class);
 });
