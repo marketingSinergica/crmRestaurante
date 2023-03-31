34367718286398c92aba3ef0716ee0b513fde849
clararubio20 <claradeasis01@gmail.com>
2023-03-31 09:44:11 +0200

    actualización sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <a href="#" class="brand-link">
-      <span class="brand-text font-weight-light">CRM</span>
+      <span class="brand-text font-weight-light">CRM restaurante</span>
   </a>
 
   <div class="sidebar">
@@ -10,7 +10,7 @@
                   <a href="#" class="nav-link">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19h3v-6h6v6h3v-9l-6-4.5L6 10v9Zm-2 2V9l8-6l8 6v12h-7v-6h-2v6H4Zm8-8.75Z"/></svg>
                       <p>
-                          Dashboard
+                          Bienvenida
                       </p>
                   </a>
               </li>
